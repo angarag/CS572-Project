633e642a5e91a5e8556eeed123773289f3d8b6ad
angarag <mars0411@gmail.com>
2019-02-05 12:16:40 -0600

    students token page: token validation added

---
+++ frontApp/src/app/student/student_service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable()
+export class StudentService {
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) { }
+    public updateToken(obj) {
+        return this.http.post('http://localhost:3600/api' + "/students/updateToken",obj);
+    }
+    public validateToken(token) {
+        return this.http.get('http://localhost:3600/api' + "/students/validateToken/"+token);
+    }
+    public sendInvitation(email){
+        return this.http.get('http://localhost:3600/api' + "/students/invite/"+email);
+    }
+}
