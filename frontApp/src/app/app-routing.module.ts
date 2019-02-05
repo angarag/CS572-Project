1d59cc7477834506fca021694ef0903699944267
Kassahun Assafw <kassamsay@gmail.com>
2019-02-05 11:45:49 -0600

    Question componenet and services added

--- frontApp/src/app/app-routing.module.ts
+++ frontApp/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './auth/login/login.component';
 import { AddStaffComponent } from './admin/add-staff/add-staff.component';
 import { DisplayStaffComponent } from './admin/display-staff/display-staff.component';
 import { HomeComponent } from './admin/home/home.component';
+import { QuestionComponent } from './admin/qusetions/qusetions.component';
 
 const routes: Routes = [
   { path:'', component: LoginComponent },
   { path:'home', component: HomeComponent },
   { path:'login', component: LoginComponent },
   { path: 'addstaff', component: AddStaffComponent},
-  { path: 'displaystaff', component: DisplayStaffComponent}
+  { path: 'displaystaff', component: DisplayStaffComponent},
+  { path: 'addsQuestion', component: QuestionComponent},
 ];
 
 @NgModule({
--- frontApp/src/app/app.module.ts
+++ frontApp/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FooterComponent } from './static/footer/footer.component';
 import { LoaderComponent } from './static/loader/loader.component';
 import { AddStaffComponent } from './admin/add-staff/add-staff.component';
 import { DisplayStaffComponent } from './admin/display-staff/display-staff.component';
-import { QusetionsComponent } from './admin/qusetions/qusetions.component';
+import { QuestionComponent } from './admin/qusetions/qusetions.component';
 import { StaffModule } from './staff/staff.module';
 import { HomeComponent } from './admin/home/home.component';
 
@@ -24,7 +24,7 @@ import { HomeComponent } from './admin/home/home.component';
     LoaderComponent,
     AddStaffComponent,
     DisplayStaffComponent,
-    QusetionsComponent,
+    QuestionComponent,
     HomeComponent
   ],
   imports: [
