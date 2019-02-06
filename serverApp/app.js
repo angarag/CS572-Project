18a15945ac440b31a02a11bffd7e482f28b7a76a
ranjan <ranjanbhusal@gmail.com>
2019-02-05 18:25:10 -0600

    minor changes

--- serverApp/app.js
+++ serverApp/app.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 //Middleware
-app.use('/api', validateToken())
+//app.use('/api', validateToken())
 
 //Routes
 app.use("/admin", require('./routes/admin'));
